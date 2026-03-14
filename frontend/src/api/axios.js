388427645ed5bdea6950b794bcc83d7d4fa0abf2
prachi737 <prachiparmar737@gmail.com>
2026-03-14 13:04:01 +0530

    resolved conflict errors

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD:frontend/src/api/axios.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Update this to your backend URL
-});
-
-// Automatically attach JWT token to every request
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-=======
 import axios from 'axios';
 
 const api = axios.create({
@@ -29,6 +12,4 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
->>>>>>> a6c281ca7deb3f1660ca29e4576ab04d335cbe31:src/api/axios.js
 export default api;
